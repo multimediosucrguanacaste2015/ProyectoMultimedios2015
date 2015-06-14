5a3a18d1d6dd1202fb1688d5d58aa1ff912f552e
cooper15 <v.centeno15@gmail.com>
2015-06-13 21:20:13 -0600

    Agregada vista de login,  mejoras visuales a la vista del login.

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Sistema control notas</title>
+    <link rel="shortcut icon" href="">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/login.css">
+
+    <style>
+        body {
+            padding-top: 50px;
+        }
+
+        .starter-template {
+            padding: 40px 15px;
+            text-align: center;
+        }
+    </style>
+
+    <!--[if IE]>
+        <script src="https://cdn.jsdelivr.net/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-1 col-md-offset-3"></div>
+            <div class="col-md-4">
+
+                <div class="panel panel-info">
+                   <div class="panel-heading"><h4>Inicie sesión</h4></div>
+                    <div class="panel-body">
+                        <form action="inserta.php" method="post">
+                            <!-- input-group e input-group-addon, permiten añadir imagenes a los campos de texto.-->
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                    <input type="text" class="form-control" id="Nombre" name="nombre" placeholder="Usuario">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                    <input type="password" class="form-control" id="Apellido1" name="Apellido1" placeholder="Contraseña">
+                                </div>
+                            </div>
+
+                            <button class="btn btn-me btn-block" type="submit">Acceder</button>
+                        </form>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+    </div>
+
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
