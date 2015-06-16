70a5769daf3cbf3d4f9485779f8e4d93581fb0fd
cooper15 <v.centeno15@gmail.com>
2015-06-15 22:03:13 -0600

    consulta con la base de datos para obtener usuario y contraseña

--- mod/login.php
+++ mod/login.php
@@ -3,6 +3,16 @@
 $nombreUsuario =  $_POST["usuario"];
 $passwordUsuario = $_POST["password"];
 
+$link = mysql_connect('localhost','root','')
+    or die ('no se pudo conectar' . mysql_error());
+mysql_select_db('proyectomultimedios')
+    or die('no se pudo conectar con la base de datos');
 
+    $consulta = "SELECT nombre_usuario, password_usuario from proyectomultimedios.usuario
+                    WHERE nombre_usuario='$nombreUsuario' and password_usuario='$passwordUsuario'";
+    $resultado = mysql_query($consulta) or die('Error en la consulta' . mysql_error());
 
+$row = mysql_fetch_array($resultado);
+    echo"$row[0]";
+    echo"$row[1]";
 ?>
