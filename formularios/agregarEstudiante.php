5d7675fc8df1d90133b3791d66b5ad89e9930507
cooper15 <v.centeno15@gmail.com>
2015-06-15 20:03:53 -0600

    agregado input-group-addon para juntar el label con el campo de texto

--- formularios/agregarEstudiante.php
+++ formularios/agregarEstudiante.php
@@ -36,61 +36,59 @@
             <div class="col-md-6">
                 <form action="inserta.php" method="post">
                     <div class="form-group">
-                       <div class="input-group">
-                           <label for="Nombre" class="input-group-addon">Nombre</label>
-                        <input type="text" class="form-control" id="Nombre" name="nombre" placeholder="Víctor">
-                       </div>
+                        <div class="input-group">
+                            <label for="Nombre" class="input-group-addon">Nombre</label>
+                            <input type="text" class="form-control" id="Nombre" name="nombre" placeholder="Víctor">
+                        </div>
                     </div>
                     <div class="form-group">
-                       <div class="input-group">
-                           <label for="Apellido" class="input-group-addon">Primer Apellido</label>
-                        <input type="text" class="form-control" id="Apellido1" name="Apellido1" placeholder="Centeno">
-                       </div>
+                        <div class="input-group">
+                            <label for="Apellido" class="input-group-addon">Primer Apellido</label>
+                            <input type="text" class="form-control" id="Apellido1" name="Apellido1" placeholder="Centeno">
+                        </div>
 
                     </div>
                     <div class="form-group">
-                       <div class="input-group">
-                           <label for="Apellido2" class="input-group-addon">Segundo Apellido</label>
-                        <input type="text" class="form-control" id="Apellido2" name="Apellido2" placeholder="Gómez">
-                       </div>
+                        <div class="input-group">
+                            <label for="Apellido2" class="input-group-addon">Segundo Apellido</label>
+                            <input type="text" class="form-control" id="Apellido2" name="Apellido2" placeholder="Gómez">
+                        </div>
 
                     </div>
                     <div class="form-group">
-                       <div class="input-group">
-                           <label for="fechaNacimiento" class="input-group-addon">Fecha de nacimiento</label>
-                        <input type="date" class="form-control" id="fechaNacimiento" name="fechaNacimiento" >
-                       </div>
+                        <div class="input-group">
+                            <label for="fechaNacimiento" class="input-group-addon">Fecha de nacimiento</label>
+                            <input type="date" class="form-control" id="fechaNacimiento" name="fechaNacimiento">
+                        </div>
 
                     </div>
                     <div class="form-group">
-                       <div class="input-group">
-                           <label for="carnet" class="input-group-addon">Carnet</label>
-                        <input type="text" class="form-control" id="carnet" name="carnet">
-                       </div>
+                        <div class="input-group">
+                            <label for="carnet" class="input-group-addon">Carnet</label>
+                            <input type="text" class="form-control" id="carnet" name="carnet" placeholder="A9999">
+                        </div>
 
                     </div>
                     <div class="form-group">
-                       <div class="input-group">
-                        <label for="telefono" class="input-group-addon">Teléfono</label>
-                        <input type="text" class="form-control" id="telefono" name="telefono" placeholder="555 555 55">
-                       </div>
+                        <div class="input-group">
+                            <label for="telefono" class="input-group-addon">Teléfono</label>
+                            <input type="text" class="form-control" id="telefono" name="telefono" placeholder="555 555 55">
+                        </div>
 
                     </div>
                     <div class="radio">
-  <label>
-    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-    Masculino
-  </label>
-</div>
-                          <div class="radio">
-  <label>
-    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
-    Femenino
-  </label>
-</div>
+                        <label>
+                            <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked> Masculino
+                        </label>
+                    </div>
+                    <div class="radio">
+                        <label>
+                            <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked> Femenino
+                        </label>
+                    </div>
                     <div class="form-group">
                         <label for="dirección"></label>
-                        <textarea class="form-control" name="dirección" id="" cols="30" rows="10">150 mts oeste de pinturas protecto</textarea>
+                        <textarea class="form-control" name="dirección" id="" cols="30" rows="3">150 mts oeste de pinturas protecto</textarea>
                     </div>
 
                     <button class="btn btn-default" type="submit">Enviar</button>
