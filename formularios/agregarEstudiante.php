683c7a91adfe9d41bc1bc3e4469009ff58705432
cooper15 <v.centeno15@gmail.com>
2015-06-26 20:02:19 -0600

    Implementación de formulario en ventana modal.

--- formularios/agregarEstudiante.php
+++ formularios/agregarEstudiante.php
@@ -10,7 +10,7 @@
     <link rel="shortcut icon" href="">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/bootstrap-theme.min.css">
-
+    <link rel="stylesheet" href="../css/main.css">
     <style>
         body {
             padding-top: 50px;
@@ -34,157 +34,94 @@
         <div class="row">
             <div class="col-md-1 col-md-offset-1"></div>
             <div class="col-md-9">
-                <form action="./mod/agregarEstudiante.php" method="post" id="form-agregar-estudiante">
-                    <div class="panel-group" id="acordion">
-                        <div class="panel panel-default">
-                            <!--Estudiante-->
-                            <div class="panel-heading">
-                                <h4 class="panel-title">
-                                <a data-toggle="collapse" data-parent="#acordion" href="#datos-estudiante">Datos de estudiante</a>
-                            </h4>
-                            </div>
-                            <div class="collapse collapse in" id="datos-estudiante">
-                                <div class="panel-body">
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <label for="Nombre" class="input-group-addon">Nombre</label>
-                                            <input type="text" class="form-control" id="Nombre" name="nombre" placeholder="Víctor">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">Datos estudiante</h4>
+                    </div>
+                    <div class="panel-body">
+                        <a href="#" class="btn btn-info"><span class="glyphicon glyphicon-plus" data-toggle="modal" data-target="#agregarNivelModal"> Agregar</span></a>
+                        <button href="#" class="btn btn-info" id="btn-refrescar"><span class="glyphicon glyphicon-refresh"> Refrescar</span>
+                        </button>
+                        <div class="container">
+                            <div class="modal fade" id="agregarNivelModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                <div class="modal-dialog" role="document">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h4 class="modal-title">Agregar Niveles</h4>
                                         </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <label for="Apellido" class="input-group-addon">Primer Apellido</label>
-                                            <input type="text" class="form-control" id="Apellido1" name="apellido1" placeholder="Centeno">
+                                        <div class="modal-body">
+
+                                            <form class="agregar-estudiante">
+                                                <div class="form-group">
+                                                    <div class="input-group">
+                                                        <label for="descripcion" class="input-group-addon">Nombre</label>
+                                                        <input type="text" class="form-control" name="nombre" id="nombre">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <div class="input-group">
+                                                        <label for="descripcion" class="input-group-addon">Apellido 1</label>
+                                                        <input type="text" class="form-control" name="apellido1" id="apellido1">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <div class="input-group">
+                                                        <label for="descripcion" class="input-group-addon">Apellido 2</label>
+                                                        <input type="text" class="form-control" name="apellido2" id="apellido2">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <div class="input-group">
+                                                        <label for="descripcion" class="input-group-addon">Carnet</label>
+                                                        <input type="text" class="form-control" name="carnet" id="carnet">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <div class="input-group">
+                                                        <label for="descripcion" class="input-group-addon">Nacimiento</label>
+                                                        <input type="date" class="form-control" name="fechaNacimiento" id="fechaNacimiento">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <div class="input-group">
+                                                        <label for="descripcion" class="input-group-addon">Telefono</label>
+                                                        <input type="text" class="form-control" name="carnet" id="carnet">
+                                                    </div>
+                                                </div>
+                                                <div class="radio">
+                                                    <label>
+                                                        <input type="radio" class="radio" name="sexo" id="sexo" value="Masculino" checked> Masculino
+                                                    </label>
+                                                </div>
+                                                <div class="radio">
+                                                    <label>
+                                                        <input type="radio" class="radio" name="sexo" id="sexo" value="Femenino" unchecked> Femenino
+                                                    </label>
+                                                </div>
+                                                <div class="form-group">
+                                                    <textarea class="form-control" name="direccion" id="" cols="10" rows="5">150 mts este del Banco Nacional de Costa Rica.</textarea>
+
+                                                </div>
+                                                <button class="btn btn-info" type="submit" id="submit" data-dismiss="modal">Enviar</button>
+                                            </form>
                                         </div>
 
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <label for="Apellido2" class="input-group-addon">Segundo Apellido</label>
-                                            <input type="text" class="form-control" id="Apellido2" name="apellido2" placeholder="Gómez">
-                                        </div>
-
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <label for="fechaNacimiento" class="input-group-addon">Fecha de nacimiento</label>
-                                            <input type="date" class="form-control" id="fechaNacimiento" name="fechaNacimiento">
-                                        </div>
-
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <label for="carnet" class="input-group-addon">Carnet</label>
-                                            <input type="text" class="form-control" id="carnet" name="carnet" placeholder="A9999">
-                                        </div>
-
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <label for="telefono" class="input-group-addon">Teléfono</label>
-                                            <input type="text" class="form-control" id="telefono" name="telefono" placeholder="555 555 55">
-                                        </div>
-
-                                    </div>
-                                    <div class="radio">
-                                        <label>
-                                            <input type="radio" class="radio" name="sexo" id="sexo" value="Masculino" checked> Masculino
-                                        </label>
-                                    </div>
-                                    <div class="radio">
-                                        <label>
-                                            <input type="radio" class="radio" name="sexo" id="sexo" value="Femenino" unchecked> Femenino
-                                        </label>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="dirección"></label>
-                                        <textarea class="form-control" name="direccion" id="direccion" cols="30" rows="3">150 mts oeste de pinturas protecto</textarea>
                                     </div>
                                 </div>
-                            </div>
 
+                            </div>
+                        </div>
+                        <div class="tabla">
 
                         </div>
                     </div>
-                    <!--Datos del padre-->
-                    <div class="panel-group" id="acordion2">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h4 class="panel-title">
-                                <a data-toggle="collapse" data-parent="#acordion2" href="#datos-padre">Datos del padre</a>
-                            </h4>
-                            </div>
-                            <div class="collapse collapse-in" id="datos-padre">
-                                <div class="panel-body">
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <label for="Nombre" class="input-group-addon">Nombre</label>
-                                            <input type="text" class="form-control" id="nombrePadre" name="nombrePadre" placeholder="Víctor">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <label for="Apellido" class="input-group-addon">Primer Apellido</label>
-                                            <input type="text" class="form-control" id="Apellido1" name="apellido1Padre" placeholder="Centeno">
-                                        </div>
-
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <label for="Apellido2" class="input-group-addon">Segundo Apellido</label>
-                                            <input type="text" class="form-control" id="Apellido2" name="apellido2Padre" placeholder="Gómez">
-                                        </div>
-
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <label for="cedula" class="input-group-addon">cedula</label>
-                                            <input type="text" class="form-control" id="cedula" name="cedula">
-                                        </div>
-
-                                    </div>
-                                    <!--                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <label for="carnet" class="input-group-addon">Carnet</label>
-                                            <input type="text" class="form-control" id="carnet" name="carnet" placeholder="A9999">
-                                        </div>
-
-                                    </div>-->
-                                    <div class="form-group">
-                                        <div class="input-group">
-                                            <label for="parentesco" class="input-group-addon">Parentesco</label>
-                                            <input type="text" class="form-control" id="parentesco" name="parentesco" placeholder="555 555 55">
-                                        </div>
-
-                                    </div>
-                                    <div class="radio">
-                                        <label>
-                                            <input type="radio" class="radio" name="sexoPadre" id="sexo" value="Masculino" checked> Masculino
-                                        </label>
-                                    </div>
-                                    <div class="radio">
-                                        <label>
-                                            <input type="radio" class="radio" name="sexoPadre" id="sexo" value="Femenino" unchecked> Femenino
-                                        </label>
-                                    </div>
-                                    <!--  <div class="form-group">
-                                        <label for="dirección"></label>
-                                        <textarea class="form-control" name="direccion" id="direccion" cols="30" rows="3">150 mts oeste de pinturas protecto</textarea>
-                                    </div>-->
-                                </div>
-                            </div>
-
-
-                        </div>
-                    </div>
-                    <button class="btn btn-default" type="submit">Enviar</button>
-                </form>
+                </div>
             </div>
         </div>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
-  <!--  <script>
+    <!--  <script>
         $(document).ready(function () {
             $('#form-agregar-estudiante').submit(function (e) {
                 e.preventDefault();
