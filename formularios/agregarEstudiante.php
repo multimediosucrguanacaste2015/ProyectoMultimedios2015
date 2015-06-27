564c0aba2d53f0cc8f179d60444c5e662110cf04
cooper15 <v.centeno15@gmail.com>
2015-06-26 21:06:25 -0600

    (Falta al principio DRY) Agregado código para visualizar la tabla Estudiantes por primera vez.

--- formularios/agregarEstudiante.php
+++ formularios/agregarEstudiante.php
@@ -112,7 +112,49 @@
                             </div>
                         </div>
                         <div class="tabla">
+                            <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Carnet</th>
+                                    <th>Nombre</th>
+                                    <th>P. Apellido</th>
+                                    <th>S. Apellido</th>
+                                    <th>F. Nacimiento</th>
+                                    <th>Sexo</th>
+                                    <th>Telefono</th>
+                                    <th>Dirección</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                   /* $link = mysql_connect('localhost','root','') or die ('no se pudo conectar' . mysql_error());*/
+                                    $conn = mysqli_connect("localhost", "root", "","multimediosdb2") or die (mysql_error ());
+                                   /* mysql_select_db('multimediosdb2') or die('no se pudo conectar con la base de datos');*/
 
+                                    // seleccionar tabla nivel y mostrarla
+                                    $consultaEstudiante = "SELECT * From multimediosdb2.Niveles";
+                                        $resultadoEstudiante = mysqli_query($conn, $consultaEstudiante) or die('Error en la consulta' . mysql_error());
+                                    if (mysqli_num_rows($resultadoNivel) > 0){
+                                        while ($columna = mysqli_fetch_row($resultadoNivel)){
+                                         echo"
+                                         <tr>
+                                            <td>$columna[0]</td>
+                                            <td>$columna[1]</td>
+                                            <td>$columna[2]</td>
+                                            <td>$columna[3]</td>
+                                            <td>$columna[4]</td>
+                                            <td>$columna[5]</td>
+                                            <td>$columna[6]</td>
+                                            <td>$columna[7]</td>
+                                            <td>$columna[8]</td>
+                                         </tr>";
+
+                                        }
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
                         </div>
                     </div>
                 </div>
