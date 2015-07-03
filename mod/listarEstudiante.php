fb1a16af826c6ae4066ed8294608a5041a4c571a
cooper15 <v.centeno15@gmail.com>
2015-07-02 21:54:38 -0600

    pase id a ventana modal exitoso

--- mod/listarEstudiante.php
+++ mod/listarEstudiante.php
@@ -6,7 +6,8 @@ mysql_select_db('multimediosdb2')
 $conn = mysqli_connect('localhost', 'root', '','multimediosdb2') or die (mysql_error ());
             $consultaEstudiante = "SELECT * From multimediosdb2.Estudiantes";
             $resultadoEstudiante = mysqli_query($conn, $consultaEstudiante) or die('Error en la consulta' . mysql_error());
- echo"<table class=\"table table-hover\">
+?>
+ <table class="table table-hover">
                     <thead>
                         <tr>
                             <th>Id</th>
@@ -19,88 +20,91 @@ $conn = mysqli_connect('localhost', 'root', '','multimediosdb2') or die (mysql_e
                             <th>Telefono</th>
                             <th>Dirección</th>
                         </tr>
-                </thead> ";
+                </thead>
+    <?php
     if(mysqli_num_rows ($resultadoEstudiante) > 0){
-        while ($columna = mysqli_fetch_row($resultadoEstudiante)){
-            echo"
+        while ($columna = mysqli_fetch_row($resultadoEstudiante)){ ?>
+
                 <tbody>
                     <tr>
-                        <td>$columna[0]</td>
-                        <td>$columna[1]</td>
-                        <td>$columna[2]</td>
-                        <td>$columna[3]</td>
-                        <td>$columna[4]</td>
-                        <td>$columna[5]</td>
-                        <td>$columna[6]</td>
-                        <td>$columna[7]</td>
-                        <td>$columna[8]</td>
+                        <td><?php echo"$columna[0]"?></td>
+                        <td><?php echo"$columna[1]"?></td>
+                        <td><?php echo"$columna[2]"?></td>
+                        <td><?php echo"$columna[3]"?></td>
+                        <td><?php echo"$columna[4]"?></td>
+                        <td><?php echo"$columna[5]"?></td>
+                        <td><?php echo"$columna[6]"?></td>
+                        <td><?php echo"$columna[7]"?></td>
+                        <td><?php echo"$columna[8]"?></td>
                         <td>
-                        <a href=\"#\" class=\"btn btn-info\"><span class=\"glyphicon glyphicon-plus\"  data-toggle=\"modal\" data-target=\"#padre\"></span></a></td>
-                         <div class=\"modal fade\" id=\"padre\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myModalLabel\" aria-hidden=\"true\">
-                                <div class=\"modal-dialog\" role=\"document\">
-                                    <div class=\"modal-content\">
-                                        <div class=\"modal-header\">
-                                            <h4 class=\"modal-title\">Agregar Padre</h4>
+
+            <a href="#" class="btn btn-info"><span class="glyphicon glyphicon-plus" data-toggle="modal" data-target="#padre<?php echo"$columna[0]"?>"></span></a>
+                        <div class="container-fluid">
+                         <div class= "modal fade " id= "padre<?php echo"$columna[0]"?>" tabindex= "-1 " role= "dialog " aria-labelledby= "myModalLabel " aria-hidden= "true ">
+                                <div class= "modal-dialog " role= "document ">
+                                    <div class= "modal-content ">
+                                        <div class= "modal-header ">
+                                            <h4 class= "modal-title ">Agregar Padre</h4>
                                         </div>
-                                        <div class=\"modal-body\">
-                                            <form class=\"agregar-padre\">
-                                                <div class=\"form-group\">
-                                                    <div class=\"input-group\">
-                                                        <label for=\"descripcion\" class=\"input-group-addon\">Nombre</label>
-                                                        <input type=\"text\" class=\"form-control\" name=\"nombre\" id=\"nombre\" value=\"$columna[1]\">
+                                        <div class= "modal-body ">
+                                            <form class= "agregar-padre ">
+                                                <div class= "form-group ">
+                                                    <div class= "input-group ">
+                                                        <label for= "descripcion " class= "input-group-addon ">Nombre</label>
+                                                        <input type= "text " class= "form-control " name= "nombre " id= "nombre ">
                                                     </div>
                                                 </div>
-                                                <div class=\"form-group\">
-                                                    <div class=\"input-group\">
-                                                        <label for=\"descripcion\" class=\"input-group-addon\">Apellido 1</label>
-                                                        <input type=\"text\" class=\"form-control\" name=\"apellido1\" id=\"apellido1\">
+                                                <div class= "form-group ">
+                                                    <div class= "input-group ">
+                                                        <label for= "descripcion " class= "input-group-addon ">Apellido 1</label>
+                                                        <input type= "text " class= "form-control " name= "apellido1 " id= "apellido1 ">
                                                     </div>
                                                 </div>
-                                                <div class=\"form-group\">
-                                                    <div class=\"input-group\">
-                                                        <label for=\"descripcion\" class=\"input-group-addon\">Apellido 2</label>
-                                                        <input type=\"text\" class=\"form-control\" name=\"apellido2\" id=\"apellido2\">
+                                                <div class= "form-group ">
+                                                    <div class= "input-group ">
+                                                        <label for= "descripcion " class= "input-group-addon ">Apellido 2</label>
+                                                        <input type= "text " class= "form-control " name= "apellido2 " id= "apellido2 ">
                                                     </div>
                                                 </div>
-                                                <div class=\"form-group\">
-                                                    <div class=\"input-group\">
-                                                        <label for=\"descripcion\" class=\"input-group-addon\">Cedula</label>
-                                                        <input type=\"text\" class=\"form-control\" name=\"cedula\" id=\"cedula\">
+                                                <div class= "form-group ">
+                                                    <div class= "input-group ">
+                                                        <label for= "descripcion " class= "input-group-addon ">Cedula</label>
+                                                        <input type= "text " class= "form-control " name= "cedula " id= "cedula ">
                                                     </div>
                                                 </div>
-                                                <div class=\"form-group\">
-                                                    <div class=\"input-group\">
-                                                        <label for=\"descripcion\" class=\"input-group-addon\">Descripcion</label>
-                                                        <input type=\"text\" class=\"form-control\" name=\"descripcion\" id=\"fechaNacimiento\">
+                                                <div class= "form-group ">
+                                                    <div class= "input-group ">
+                                                        <label for= "descripcion " class= "input-group-addon ">Descripcion</label>
+                                                        <input type= "text " class= "form-control " name= "descripcion " id= "fechaNacimiento ">
                                                     </div>
-                                                </div><div class=\"form-group\">
-                                                    <div class=\"input-group\">
-                                                        <label for=\"ocupacion\" class=\"input-group-addon\">Ocupacion</label>
-                                                        <input type=\"text\" class=\"form-control\" name=\"ocupacion\" id=\"fechaNacimiento\">
+                                                </div><div class= "form-group ">
+                                                    <div class= "input-group ">
+                                                        <label for= "ocupacion " class= "input-group-addon ">Ocupacion</label>
+                                                        <input type= "text " class= "form-control " name= "ocupacion " id= "fechaNacimiento ">
                                                     </div>
                                                 </div>
-                                                <div class= \"form-group\">
-                                                    <div class= \"input-group\">
-                                                        <label class=\"input-group-addon\">Sexo</label>
-                                                        <select class= \"form-control\">
+                                                <div class=  "form-group ">
+                                                    <div class=  "input-group ">
+                                                        <label class= "input-group-addon ">Sexo</label>
+                                                        <select class=  "form-control ">
                                                             <option>M</option>
                                                             <option>F</option>
                                                         </select>
                                                     </div>
                                                 </div>
 
-                                                <div class=\"form-group\">
-                                                    <div class=\"input-group\">
-                                                        <label for=\"descripcion\" class=\"input-group-addon\">Telefono</label>
-                                                        <input type=\"text\" class=\"form-control\" name=\"carnet\" id=\"carnet\">
+                                                <div class= "form-group ">
+                                                    <div class= "input-group ">
+                                                        <label for= "descripcion " class= "input-group-addon ">Telefono</label>
+                                                        <input type= "text " class= "form-control " name= "carnet " id= "carnet ">
                                                     </div>
                                                 </div>
 
-                                                <div class=\"form-group\">
-                                                    <textarea class=\"form-control\" name=\"direccion\"  cols=\"10\" rows=\"5\">150 mts este del Banco Nacional de Costa Rica.</textarea>
+                                                <div class= "form-group ">
+                                                    <textarea class= "form-control " name= "direccion "  cols= "10 " rows= "5 ">150 mts este del Banco Nacional de Costa Rica.</textarea>
 
                                                 </div>
-                                                <button class= \"btn btn-info\" onclick=\"agregarPadre()\" type=\"submit\" id=\"submit\" data-dismiss=\"modal\">Enviar</button>
+                                                <button class=  "btn btn-info " onclick= "agregarPadre() " type= "submit " id= "submit " data-dismiss= "modal ">Enviar</button>
                                             </form>
 
 
@@ -110,14 +114,17 @@ $conn = mysqli_connect('localhost', 'root', '','multimediosdb2') or die (mysql_e
                                 </div>
 
                             </div>
-
+     </div>
+                            </td>
                     </tr>
-                </tbody>" ;
+     </tbody>
 
 
+<?php
         }
 
     }
-  echo"</table>";
-
+?>
+</table>
+<?php
 ?>
