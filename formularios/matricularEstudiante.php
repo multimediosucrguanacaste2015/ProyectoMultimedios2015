e90f87d3e074dbde03017d7dd7f4e4f7fb0fdc91
cooper15 <v.centeno15@gmail.com>
2015-07-10 16:24:59 -0600

    matricularEstudiantes cambio

--- formularios/matricularEstudiante.php
+++ formularios/matricularEstudiante.php
@@ -46,14 +46,18 @@
                 </select><?php
                 ?>
           </div>
-          <button class="btn btn-info" onclick="matricular()">Enviar</button>
+
       </form>
+      <button class="btn btn-info" id="boton" onclick="matricular()">Enviar</button>
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script>
         function matricular(){
+            $("#boton").click(function(event){
+                event.preventDefault();
+            });
             $.ajax({
                 type:"post",
                 // La url varia dependiendo desde donde se ejecute el formulario, en esta caso se prevee su funcionamiento desde el dashboard
